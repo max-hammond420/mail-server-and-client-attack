92ad557320d10daac8008cb3a7febd611cdd9c40
Max <max.hammond21@gmail.com>
2022-10-28 20:42:22 +1100

    added id

--- client.py
+++ client.py
@@ -4,8 +4,8 @@ import sys
 
 
 # Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
-PERSONAL_ID = ''
-PERSONAL_SECRET = ''
+PERSONAL_ID = 'F8D819'
+PERSONAL_SECRET = '44c42ab54ed4c444130f09261509f85b'
 
 
 def main():
