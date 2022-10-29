623cc28da0266ae8e68895ed8d67d577d7560324
Max <max.hammond21@gmail.com>
2022-10-29 20:55:26 +1100

    test

--- client.py
+++ client.py
@@ -10,6 +10,12 @@ PERSONAL_SECRET = '44c42ab54ed4c444130f09261509f85b'
 
 def main():
     # TODO
+    # 1. Create a socket
+    # 2. Connect to the server
+    # 3. Send the personal ID and secret to the server
+    # 4. Receive the response from the server
+    # 5. Print the response
+    # 6. Close the socket
     pass
 
 
--- server.py
+++ server.py
@@ -4,12 +4,17 @@ import sys
 
 
 # Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
-PERSONAL_ID = ''
-PERSONAL_SECRET = ''
+PERSONAL_ID = 'F8D819'
+PERSONAL_SECRET = '44c42ab54ed4c444130f09261509f85b'
 
 
 def main():
     # TODO
+    # prepare for any incoming client connection
+    # receive emails from a client and save to disk
+    # additionally allow client authentication
+    # allow multiple clients to connect simultaniously
+    # terminate upon receiving a SIGNT signal
     pass
 
 
