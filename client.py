fa252f38613009c7670db99f355cba04949c7553
Max <max.hammond21@gmail.com>
2022-10-31 16:56:26 +1100

    test

--- client.py
+++ client.py
@@ -96,10 +96,11 @@ def main():
     with open(conf) as f:
         lines = f.readlines()
 
-    print(lines)
 
     for i in range(len(lines)):
         lines[i] = lines[i].strip()
+    
+    print(f"c: lines")
 
     conf = conv_dict(lines, '=')
 
