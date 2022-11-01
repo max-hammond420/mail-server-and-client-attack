05657437ed7e92cacbaf2228920c46608dd514e3
Max <max.hammond21@gmail.com>
2022-11-01 16:10:34 +1100

    debug

--- client.py
+++ client.py
@@ -116,6 +116,7 @@ def main():
     # print(os.listdir(send_path))
     print(os.listdir(send_path))
     for filename in os.listdir(send_path):
+        print(filename)
         path = os.path.join(send_path[1:], filename)
         path = path[1:]
         print(path)
