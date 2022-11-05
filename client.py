0ab8d73254958edeeb2811ff6677476d6c9f0fb6
Max <max.hammond21@gmail.com>
2022-11-05 19:22:43 +1100

    server email regular expression

--- client.py
+++ client.py
@@ -9,7 +9,6 @@ import sys
 # Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
 PERSONAL_ID = 'F8D819'
 PERSONAL_SECRET = '44c42ab54ed4c444130f09261509f85b'
-
 PERSONAL_SECRET_MD5 = hashlib.md5(PERSONAL_SECRET.encode())
 # print(PERSONAL_SECRET_MD5.digest())
 
