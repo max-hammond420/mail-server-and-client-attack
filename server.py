12d30cb0d28219a31daacb05269fec18022c5be1
Max <max.hammond21@gmail.com>
2022-11-05 14:03:50 +1100

    server response fix

--- server.py
+++ server.py
@@ -163,27 +163,31 @@ def server(HOST, PORT, checkpoints):
 
                 # print("\\n ", data[-1] == '\n')
                 # print("\\r ", data[-2] == '\r')
-                # check if client msg contains \r\n and data[-3] is alpha
+                # check if client msg contains \r\n and data[-3]is alpha
                 if not data[-3].isalnum() or data[-2]!='\r' or data[-1]!='\n':
                     response = "501 Syntax error in parameters or arguments"
                     print(f"S: {response}\r\n", end='', flush=True)
                     conn.send((response+'\r\n').encode())
                     continue
+
                 data = data.strip()
                 data = data.split()
+
                 if data[0] == "QUIT":
                     response = "221 Service closing transmission channel"
                     print(f"S: {response}\r\n", end='', flush=True)
                     conn.send((response+'\r\n').encode())
                     continue
+
                 response, checkpoints, rcpt_check = server_response(data, checkpoints, rcpt_check)
 
                 # Check authentication
                 if checkpoints['EHLO'] is True and checkpoints['MAIL'] is False:
                     pass
 
-                # print(response)
-                # print(checkpoints)
+                if '\r\n' in response:
+                    response = response.split('\r\n')
+                    print(f"S: {response[0]}\r\nS: {response[1]}\r\n")
                 print(f"S: {response}\r\n", end='')
 
                 conn.send((response+'\r\n').encode())
