db14ecb61107d0b739dbb322b493608f56f88004
Max <max.hammond21@gmail.com>
2022-11-06 20:02:57 +1100

    email regex check

--- test.py
+++ test.py
@@ -26,21 +26,6 @@ def check_email(prefix, data):
     email = None
     data = data.split(':')
 
-    # let_dig = fr'[a-zA-Z0-9]'
-    # ldh_str = fr'({let_dig}|-) {let_dig}'
-    # atom = fr'{let_dig} *({let_dig}|-)'
-    # dot_string = fr'{atom} *(.{atom})'
-    # sub_domain = fr'let_dig [{ldh_str}]'
-    # domain = fr'{sub_domain} +(.{sub_domain})'
-    # mailbox = fr'<{dot_string}@{domain}>'
-    # email_regex = re.compile(mailbox)
-
-    # let_dig = "a-zA-Z0-9"
-
-    # atom = f"[{let_dig}][{let_dig}-]*"
-    # dot_string = f"[{atom}][.{atom}]*"
-    # mailbox = f"{dot_string}@{domain}"
-
     let_dig = 'a-zA-Z0-9'
     ldh_str = f'[{let_dig}-]*[{let_dig}]'
     atom = f'[{let_dig}][{let_dig}-]*'
@@ -67,4 +52,4 @@ print(check_email('TO', 'TO:<bob@bob.org>'))
 print(check_email('TO', 'TO:<bob@bob.org.org.org>'))
 print(check_email('TO', 'TO: <bob@bob.org'))
 print(check_email('TO', 'TO:<boborg>'))
-print(check_email('TO', 'TO:<-bob.org>'))
+print(check_email('TO', 'TO:<bob@-bob.org>'))
