296ab7b3ccf5ca8b3c8d465a573d4b972c745d72
Max <max.hammond21@gmail.com>
2022-11-08 19:30:03 +1100

    working on auth

--- server.py
+++ server.py
@@ -307,35 +307,40 @@ def server(HOST, PORT, checkpoints, file):
                 data = conn.recv(1024).decode()
 
                 # AUTH
-                if data == "AUTH CRAM-MD5\r\n":
-                    challenge = generate_challenge()
-                    response = f"334 {challenge}"
-                    # print(response)
-                    print(f"S: {response}\r\n", end='', flush=True)
-                    conn.send((response+'\r\n').encode())
-
-                    data = conn.recv(1024).decode()
-
-                    a = compute_digest(challenge)
-                    print(a)
-                    print(data.strip())
-                    if data.strip() == a.strip():
-                        print("yes cunt")
-                    while data.strip() != a.strip():
-                        response = "535 Authentication credentials invalid"
+                while True:
+                    if data == "AUTH CRAM-MD5\r\n":
+                        challenge = generate_challenge()
+                        response = f"334 {challenge}"
+                        # print(response)
                         print(f"S: {response}\r\n", end='', flush=True)
                         conn.send((response+'\r\n').encode())
-                        data = conn.recv(1024).decode()
-                        print(f"client: {data.strip()}")
-                        if data.strip() == "QUIT":
-                            data = data.strip()
-                            break
-                        print(f"C: {data}", end='', flush=True)
-                    response = "235 Authentication successful\r\n"
-                    print(f"S: {response}\r\n", end='', flush=True)
-                    conn.send((response+'\r\n').encode())
 
-                    continue
+                        data = conn.recv(1024).decode()
+
+                        a = compute_digest(challenge)
+                        print(a)
+                        print(data.strip())
+                        if data.strip() == "AUTH CRAM-MD5":
+                            continue
+                        if data.strip() == a.strip():
+                            print("yes cunt")
+                        while data.strip() != a.strip():
+                            response = "535 Authentication credentials invalid"
+                            print(f"S: {response}\r\n", end='', flush=True)
+                            conn.send((response+'\r\n').encode())
+                            data = conn.recv(1024).decode()
+                            print(f"client: {data.strip()}")
+                            if data.strip() == "QUIT":
+                                data = data.strip()
+                                break
+                            print(f"C: {data}", end='', flush=True)
+                        response = "235 Authentication successful\r\n"
+                        print(f"S: {response}\r\n", end='', flush=True)
+                        conn.send((response+'\r\n').encode())
+
+                        continue
+                    else:
+                        break
 
                 # If no client says nothing, do nothing
                 if not data:
