a8b2bf4015431360eeda3e9f40609950e0e5c1da
Max <max.hammond21@gmail.com>
2022-11-08 14:10:11 +1100

    working on autH

--- server.py
+++ server.py
@@ -96,7 +96,10 @@ def generate_challenge():
     # returns a string that is 16 <= len <= 128 bytes
     bytes = random.randint(16, 128)
     challenge = secrets.token_hex(bytes)
-    return challenge
+    challenge = challenge.encode('ascii')
+    base64_bytes = base64.b64encode(challenge)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
 
 
 def check_ipv4(ip):
